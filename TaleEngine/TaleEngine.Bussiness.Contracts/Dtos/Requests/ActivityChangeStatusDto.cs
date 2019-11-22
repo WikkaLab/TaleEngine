19be4665b3d7a174d9aed1802947591d3381b738
Elena Guzman <elena.guzbla@gmail.com>
2019-11-22 22:13:39 +0100

    Create ChangeActivityStatus call (end to end)

---
+++ TaleEngine/TaleEngine.Bussiness.Contracts/Dtos/Requests/ActivityChangeStatusDto.cs
@@ -0,0 +1,8 @@
+﻿namespace TaleEngine.Bussiness.Contracts.Dtos.Requests
+{
+    public class ActivityChangeStatusDto
+    {
+        public int ActivityId { get; set; }
+        public int StatusId { get; set; }
+    }
+}
