5c719880d783bdb9ec5e2adfec3c9cb2cda0d58d
ElenaDotNet <elena.guzbla@gmail.com>
2021-02-19 21:31:36 +0100

    📝 Add docs to Roles Controller

--- TaleEngine/TaleEngine/Controllers/RolesController.cs
+++ TaleEngine/TaleEngine/Controllers/RolesController.cs
@@ -1,28 +1,58 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
 using System;
+using System.Collections.Generic;
+using TaleEngine.Application.Contracts.Dtos;
 using TaleEngine.Application.Contracts.Services;
 
 namespace TaleEngine.Controllers
 {
+    /// <summary>
+    /// Roles management
+    /// </summary>
+    [ApiController]
+    [ProducesResponseType(StatusCodes.Status400BadRequest)]
+    [ProducesResponseType(StatusCodes.Status401Unauthorized)]
+    [ProducesResponseType(StatusCodes.Status403Forbidden)]
+    [ProducesResponseType(StatusCodes.Status404NotFound)]
+    [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+    [ControllerName("Management roles and permissions associated")]
     [Route("api/[controller]")]
     public class RolesController : Controller
     {
         private IRoleService _roleService;
 
+        /// <summary>
+        /// Constructor for roles controller
+        /// </summary>
+        /// <param name="roleService">Role service</param>
         public RolesController(IRoleService roleService)
         {
             _roleService = roleService ?? throw new ArgumentNullException(nameof(roleService));
         }
 
+        /// <summary>
+        /// Gets all the roles in the app
+        /// </summary>
+        /// <returns>All the roles</returns>
         [HttpGet("[action]")]
-        public IActionResult GetAllRoles()
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status204NoContent)]
+        public ActionResult<List<RoleDto>> GetAllRoles()
         {
             var result = _roleService.GetAllRoles();
 
             return Ok(result);
         }
 
+        /// <summary>
+        /// Gets a single role with permissions
+        /// </summary>
+        /// <param name="roleId">Role identifier</param>
+        /// <returns>The selected role with permissions</returns>
         [HttpGet("[action]/{roleId:int}")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status204NoContent)]
         public IActionResult GetRole(int roleId)
         {
             return Ok();
