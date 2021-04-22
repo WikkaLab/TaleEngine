f0fdd02927ac8b3fb95b59f4aa152bd912e6e057
David Oliva Paredes <d.oliva@prodware.es>
2021-04-22 22:57:21 +0200

    Add ActivityRepositoryTests

---
+++ TaleEngine/TaleEngine.IntegrationTests/Fixtures/DatabaseContextInMemoryDatabase.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using TaleEngine.Data;
+
+namespace TaleEngine.IntegrationTests
+{
+    public class DatabaseContextInMemoryDatabase
+    {
+        protected DatabaseContext _context;
+
+        public DatabaseContextInMemoryDatabase()
+        {
+            var options = CreateNewContextOptions();
+
+            _context = new DatabaseContext(options);
+        }
+
+        protected static DbContextOptions<DatabaseContext> CreateNewContextOptions()
+        {
+            // Create a fresh service provider, and therefore a fresh
+            // InMemory database instance.
+            var serviceProvider = new ServiceCollection()
+                .AddEntityFrameworkInMemoryDatabase()
+                .BuildServiceProvider();
+
+            // Create a new options instance telling the context to use an
+            // InMemory database and the new service provider.
+            var builder = new DbContextOptionsBuilder<DatabaseContext>();
+            builder.UseInMemoryDatabase("TaleEngine")
+                   .UseInternalServiceProvider(serviceProvider);
+
+            return builder.Options;
+        }
+    }
+}
--- TaleEngine/TaleEngine/Program.cs
+++ TaleEngine/TaleEngine/Program.cs
@@ -1,20 +1,40 @@
+using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
+using System.IO;
 
 namespace TaleEngine
 {
     public class Program
     {
+        public static readonly string Namespace = typeof(Program).Namespace;
+        public static readonly string AppName = Namespace.Substring(Namespace.LastIndexOf('.', Namespace.LastIndexOf('.') - 1) + 1);
+
         public static void Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+            var configuration = GetConfiguration();
+
+            var host = BuildWebHost(configuration, args);
+            host.Run();
         }
 
-        public static IHostBuilder CreateHostBuilder(string[] args) =>
-             Host.CreateDefaultBuilder(args)
-                 .ConfigureWebHostDefaults(webBuilder =>
-                 {
-                     webBuilder.UseStartup<Startup>();
-                 });
+        private static IWebHost BuildWebHost(IConfiguration configuration, string[] args) =>
+           WebHost.CreateDefaultBuilder(args)
+               .CaptureStartupErrors(false)
+               .ConfigureAppConfiguration(x => x.AddConfiguration(configuration))
+               .UseStartup<Startup>()
+               .UseContentRoot(Directory.GetCurrentDirectory())
+               .Build();
+
+        private static IConfiguration GetConfiguration()
+        {
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+                .AddEnvironmentVariables();
+
+            return builder.Build();
+        }
     }
 }
