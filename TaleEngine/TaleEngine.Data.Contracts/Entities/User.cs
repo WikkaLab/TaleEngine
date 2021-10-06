3ec8de933ca74ae1d596e6ee6a0c1c9579c7fdd4
Elena G Blanco <elena.guzbla@gmail.com>
2021-10-06 16:26:56 +0200

    🗃 Add username field in User entity

--- TaleEngine/TaleEngine.Data.Contracts/Entities/User.cs
+++ TaleEngine/TaleEngine.Data.Contracts/Entities/User.cs
@@ -4,6 +4,7 @@ namespace TaleEngine.Data.Contracts.Entities
 {
     public class User : BaseEntity
     {
+        public string Username { get; set; }
         public string Name { get; set; }
         public string Mail { get; set; }
         public string Password { get; set; }
