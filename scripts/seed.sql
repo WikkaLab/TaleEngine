7b30d07a152f0424b1fcd47dcfce3654be9df2e6
Elena G Blanco <elena.guzbla@gmail.com>
2023-03-21 18:57:57 +0100

    🔨 Add more initial data

--- scripts/seed.sql
+++ scripts/seed.sql
@@ -1,10 +1,10 @@
-insert into Event (CreateDateTime, Title) values 
+insert into [TaleEngine].[dbo].[Event] (CreateDateTime, Title) values 
     (GETDATE(), 'Jornadas dotnet')
 
-insert into Edition (CreateDateTime, DateInit, DateEnd, EventId) values 
+insert into [TaleEngine].[dbo].[Edition] (CreateDateTime, DateInit, DateEnd, EventId) values 
     (GETDATE(), '2023-01-01', '2023-02-02', 1)
 
-insert into Activity (CreateDateTime, Title, Places, StatusId, TypeId, EditionId, TimeSlotId) values
+insert into [TaleEngine].[dbo].[Activity] (CreateDateTime, Title, Places, StatusId, TypeId, EditionId, TimeSlotId) values
 	(GETDATE(), 'Partida 1', 3, 2, 1, 1, 1),
 	(GETDATE(), 'Partida 2', 4, 2, 1, 1, 1),
 	(GETDATE(), 'Partida 3', 10, 1, 1, 1, 1),
@@ -16,3 +16,20 @@ insert into Activity (CreateDateTime, Title, Places, StatusId, TypeId, EditionId
 	(GETDATE(), 'LARP 2', 4, 2, 5, 1, 1),
 	(GETDATE(), 'LARP 3', 4, 1, 5, 1, 1),
 	(GETDATE(), 'LARP 4', 4, 3, 5, 1, 1)
+
+insert into [TaleEngine].[dbo].[User] (CreateDateTime, Name, Mail, StatusId, Username) values
+	(GETDATE(), 'User1', 'user1@email.com', 2, 'SomeUser')
+
+insert into [TaleEngine].[dbo].[FavActivities] (ActivitiesFavId, UsersFavId) values
+    (1, 1),
+    (5, 1),
+    (8, 1)
+
+--select * from [TaleEngine].[dbo].[Event]
+--select * from [TaleEngine].[dbo].[Edition]
+--select * from [TaleEngine].[dbo].[ActivityType]
+--select * from [TaleEngine].[dbo].[ActivityStatus]
+--select * from [TaleEngine].[dbo].[Activity]
+--select * from [TaleEngine].[dbo].[User]
+--select * from [TaleEngine].[dbo].[UserStatus]
+--select * from [TaleEngine].[dbo].[FavActivities]
