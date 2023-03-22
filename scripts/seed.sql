7d13d0e82213d17175cbe3eb6042263d5fe1b8e1
Elena G Blanco <elena.guzbla@gmail.com>
2023-03-22 22:04:25 +0100

    🔨 Add users data

--- scripts/seed.sql
+++ scripts/seed.sql
@@ -18,12 +18,37 @@ insert into [TaleEngine].[dbo].[Activity] (CreateDateTime, Title, Places, Status
 	(GETDATE(), 'LARP 4', 4, 3, 5, 1, 1)
 
 insert into [TaleEngine].[dbo].[User] (CreateDateTime, Name, Mail, StatusId, Username) values
-	(GETDATE(), 'User1', 'user1@email.com', 2, 'SomeUser')
+	(GETDATE(), 'User1', 'user1@email.com', 2, 'SomeUser'),
+	(GETDATE(), 'User2', 'user2@email.com', 2, 'AnotherUser'),
+	(GETDATE(), 'User3', 'user3@email.com', 2, 'ExUser'),
+	(GETDATE(), 'User4', 'user4@email.com', 1, 'AUser'),
+	(GETDATE(), 'User5', 'user5@email.com', 3, 'SomewhatUser'),
+	(GETDATE(), 'User6', 'user6@email.com', 4, 'SuperUser'),
+	(GETDATE(), 'User7', 'user7@email.com', 5, 'TheUser'),
+	(GETDATE(), 'User8', 'user8@email.com', 2, 'DaUser'),
+	(GETDATE(), 'User9', 'user9@email.com', 2, 'UserSomething'),
+	(GETDATE(), 'User10', 'user10@email.com', 2, 'ThingyUser'),
 
 insert into [TaleEngine].[dbo].[FavActivities] (ActivitiesFavId, UsersFavId) values
+    (1, 8),
+    (1, 9),
+    (5, 9),
+    (8, 10)
+
+insert into [TaleEngine].[dbo].[ActivityCreators] (ActivitiesCreateId, UsersCreateId) values
     (1, 1),
-    (5, 1),
-    (8, 1)
+    (1, 2),
+    (2, 1),
+    (3, 1),
+    (4, 2),
+    (5, 2),
+    (6, 2),
+    (7, 2),
+    (8, 3),
+    (8, 2),
+    (9, 3),
+    (10, 3),
+    (11, 3),
 
 --select * from [TaleEngine].[dbo].[Event]
 --select * from [TaleEngine].[dbo].[Edition]
